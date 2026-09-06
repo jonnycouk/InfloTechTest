90096c3b4cd74c2b5d8e74b3d64b255d0d7866cc
Jonny <jonny@jonny.uk>
2026-09-06 17:05:22 +0100

    WIP: Styling/layout changes

--- UserManagement.Web/Controllers/LogsController.cs
+++ UserManagement.Web/Controllers/LogsController.cs
@@ -13,7 +13,7 @@ public class LogsController(ILogService logService) : Controller
         ViewData["Title"] = "Log Viewer";
         IEnumerable<LogListItemViewModel> items;
         
-        items = logService.GetAll(0, 50).Select(user => new LogListItemViewModel
+        items = logService.GetAll(0, 100).Select(user => new LogListItemViewModel
         {
             Id = user.Id,
             Summary = user.Summary,
